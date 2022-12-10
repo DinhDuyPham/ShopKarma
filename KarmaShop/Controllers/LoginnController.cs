a80ff5e48256884cdadf047ba3bca1c8c056b9d5
Dinh Duy <0366010143duy@gmail.com>
2022-12-10 16:03:38 +0700

    cmm

--- KarmaShop/Controllers/LoginnController.cs
+++ KarmaShop/Controllers/LoginnController.cs
@@ -18,7 +18,7 @@ namespace KarmaShop.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Index(string Acc, string Pass)
         {
-            bool isAuthentic = (Acc != null && Pass != null && ((Acc.Equals("ADMIN") && Pass.Equals("234")) || (Acc.Equals("DUY") && Pass.Equals("234")))) ;
+            bool isAuthentic = (Acc != null && Pass != null && ((Acc.ToLower().Equals("admin") && Pass.Equals("234")) || (Acc.ToLower().Equals("duy") && Pass.Equals("234")))) ;
             if (isAuthentic)
                 return View("~/Areas/Admin/Views/DashBoard/Index.cshtml");
                            
